b338ee4971f61a51d58e090b574fe677c911649a
williamjones <willj836@gmail.com>
2023-03-04 10:33:37 +0000

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const getUrbanAreas = require("./controller");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/:continentId", getUrbanAreas);
 
